3c5885020d11e68d525b770981808682af62f3a5
Ahmed Gharib <64174723+ahmed-gharib89@users.noreply.github.com>
2020-08-28 12:18:59 +0200

    feat: add 200 paragraphs in a div and add it to the buttom of the page and add event listner to log that the paragraph was clicked

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-document.addEventListener('click', function(){
-    const mainHeading = document.querySelector('h1');
-    mainHeading.style.backgroundColor = '#00ffff'
+document.addEventListener('click', function(event){
+    console.log('x: ' + event.pageX + ', y: ' + event.pageY)
 });
 
 document.body.addEventListener('keypress', function(){
     console.log('Removing first child');
     document.querySelector('#contain-all').firstElementChild.remove()
 });
+
+const myCustomDiv = document.createElement('div');
+function respondToClick(){
+    console.log('A paragraph was clicked.');
+};
+
+for (let i=1; i<=200; i++){
+    const newEelement = document.createElement('p');
+    newEelement.textContent = 'This is paragraph number' + i;
+
+    myCustomDiv.appendChild(newEelement);
+};
+myCustomDiv.addEventListener('click', respondToClick);
+document.body.appendChild(myCustomDiv);
